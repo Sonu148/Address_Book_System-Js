b25bb41984b8ec98c70f3bd6b242781cc9a9d8a0
Sonu Kumar <sonukumar31815@gmail.com>
2025-03-08 22:51:15 +0530

    [Sonu]Add. ability to sort the entries in the address book alphabeticallt by peraon name

--- Model/Contact.js
+++ Model/Contact.js
@@ -9,6 +9,14 @@ class Contact {
         this.phone = phone;
         this.email = email;
     }
+
+    toString() {
+        return `Name: ${this.firstName} ${this.lastName}\nAddress: ${this.address}\nCity: ${this.city}\nState: ${this.state}\nZip: ${this.zip}\nPhone: ${this.phone}\nEmail: ${this.email}`;
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
 }
 
 module.exports = Contact;
