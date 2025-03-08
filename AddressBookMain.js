e5490699cebdaf9a4c48d614cf9be6975c495aa0
Sonu Kumar <sonukumar31815@gmail.com>
2025-03-08 21:36:02 +0530

    [Sonu]Add.ability to validate the contact details

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -3,9 +3,18 @@ const Contact = require('./Model/Contact');
 
 const addressBook = new AddressBook();
 
+// Example contact with valid details
 const contact1 = new Contact(
-    "Aman", "Singh", "Basant kunj ,Bhopal", "Bhopal", "India", "462022", "9876543210", "Amansingh@gmail.com"
+    "Aman", "Singh", "Basant kunj, Bhopal", "Bhopal", "India", "462022", "9876543210", "Amansingh@gmail.com"
 );
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
 
+// Example of adding a contact and checking the result
+console.log(addressBook.addContact(contact1)); 
+
+// Example of invalid contact
+const contact2 = new Contact(
+    "anmol", "singh", "Basant kunj, Bhopal", "Bhopal", "India", "462022", "9876543210", "invalidemail"
+);
+console.log(addressBook.addContact(contact2)); 
+
+console.log(addressBook.getAllContacts());
